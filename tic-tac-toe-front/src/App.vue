b9b406935665225f7f3483de52cf150483804843
losimen <koval.pavlo03@gmail.com>
2023-10-10 22:38:58 +0300

    trying to install serialport

--- tic-tac-toe-front/src/App.vue
+++ tic-tac-toe-front/src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <h1 class="text-3xl"> Hello world </h1>
+  <MainMenu/>
 </template>
 
 <script>
+import MainMenu from "@/components/MainMenu.vue";
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {MainMenu}
 }
 </script>
---
+++ tic-tac-toe-front/src/components/MainMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Hello from main</h1>
+  <button @click="onClick">Count is: {{ count }}</button>
+</template>
+
+
+<script setup lang="ts">
+import {ref} from "vue";
+import {sayHi} from "../../utils/serialCommunication";
+
+let count = ref(0);
+
+function onClick() {
+  sayHi();
+  count.value += 1;
+}
+
+</script>
+
