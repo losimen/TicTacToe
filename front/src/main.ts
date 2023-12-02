ce3854bc83f29b8853fde7af80be1d3d65921ed8
losimen <koval.pavlo03@gmail.com>
2023-12-02 18:28:43 +0200

    save font-awesome-icons

--- front/src/main.ts
+++ front/src/main.ts
@@ -3,14 +3,17 @@ import "./style.css"
 import App from './App.vue'
 import './samples/node-api'
 
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-// import {faUserSecret, faX, faO, faUser, faComputer} from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {faUserSecret, faX, faO, faUser, faComputer} from '@fortawesome/free-solid-svg-icons'
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
+library.add(faUserSecret, faX, faO, faUser, faComputer)
+
 createApp(App)
   .use(Toast, {})
+  .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app')
   .$nextTick(() => {
     postMessage({ payload: 'removeLoading' }, '*')
